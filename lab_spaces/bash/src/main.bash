7f94838fbcda807dd0eb4dfc01251608476cee81
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-20 22:17:41 +0000

    Update comment in bash

--- lab_spaces/bash/src/main.bash
+++ lab_spaces/bash/src/main.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lab space for Bash
 # Use this space to experiment with Bash
-# To run this code run "make run" in the terminal
+# To run this code run "make" in the terminal
 # Github: https://github.com/awesomelewis2007/lab_space
 
 echo "Hello World"
